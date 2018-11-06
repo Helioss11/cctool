f1d09979509bffbc930ce85d2dce5ec6fd3a9232
Helios Figueroa <hfigueroa@abargon.com>
2018-11-06 08:27:59 -0600

    fix

--- routes/assets.js
+++ routes/assets.js
@@ -6,7 +6,7 @@ var multer = require('multer');
 
 var storage = multer.diskStorage({
     destination: (req, file, cb) => {
-      cb(null, __dirname + '/public/images/uploads')
+      cb(null, path.join(__dirname, '../', '/public/images/uploads'))
     },
     filename: (req, file, cb) => {
       cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname))
