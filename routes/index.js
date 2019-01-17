16e43504d816c6832bd38be1c3ba13111182f513
Helios Figueroa <hfigueroa@abargon.com>
2019-01-17 10:59:41 -0600

    options

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ var router = express.Router();
 var JWToken = require('./auth');
 
 router.all('*', function(req, res, next){
-  if( (req.method !== 'POST' && req.url === '/api/v1/users') || 
+  if( req.method !== 'OPTIONS' || (req.method !== 'POST' && req.url === '/api/v1/users') || 
     (req.url !== '/' && req.url !== '/api/v1/users/auth' && req.url !== '/token' && req.url !== '/api/v1/users' && req.url !== '/api/v1/assets' && 
     req.url !== '/api/v1/users/username' && req.url !== '/api/v1/lookup/country' && req.url !== '/api/v1/lookup/category' && req.url !== '/api/v1/lookup/rol') ){
     let token = (req.method === 'POST' || req.method === 'PUT') ? req.body.token : req.query.token
