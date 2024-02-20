33d568492e81f19ea38a9e06c62c79d86ed4a2b9
Helios <helioss.figueroa@gmail.com>
2024-02-20 13:53:47 -0600

    fix

--- database.js
+++ database.js
@@ -1,32 +1,33 @@
-var mysql = require('mysql');
+var mysql = require("mysql");
 
 var pool = mysql.createPool({
-    connectionLimit: 10,
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'cctool_db',
-    insecureAuth: true
+  connectionLimit: 10,
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "cctool_db",
+  insecureAuth: true,
 });
 
 pool.getConnection((err, connection) => {
-    
-    if (err) {
-        if (err.code === 'PROTOCOL_CONNECTION_LOST') {
-            console.error('Database connection was closed.');
-        }
-        if (err.code === 'ER_CON_COUNT_ERROR') {
-            console.error('Database has too many connections.');
-        }
-        if (err.code === 'ECONNREFUSED') {
-            console.error('Database connection was refused.');
-        }
+  if (err) {
+    if (err.code === "PROTOCOL_CONNECTION_LOST") {
+      console.error("Database connection was closed.");
     }
+    if (err.code === "ER_CON_COUNT_ERROR") {
+      console.error("Database has too many connections.");
+    }
+    if (err.code === "ECONNREFUSED") {
+      console.error("Database connection was refused.");
+    }
+  }
 
-    if(connection) connection.release();
+  if (connection) {
+    console.log("Database connected");
+    connection.release();
+  }
 
-    return;
-
+  return;
 });
 
 module.exports = pool;
